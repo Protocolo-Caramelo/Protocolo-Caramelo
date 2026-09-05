3dfff92fcd870084661652b2edc83f7f35a2b112
Cristiano <cristianochincarosanches@gmail.com>
2026-09-05 01:06:36 -0300

    add basic setup for web-site development

---
+++ web/Protocolo-Caramelo/src/main/java/com/example/Protocolo_Caramelo/ProtocoloCarameloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Protocolo_Caramelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocoloCarameloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocoloCarameloApplication.class, args);
+	}
+
+}
---
+++ web/Protocolo-Caramelo/src/test/java/com/example/Protocolo_Caramelo/ProtocoloCarameloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Protocolo_Caramelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtocoloCarameloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
